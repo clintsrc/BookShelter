c9d074715149206549efc24c529e1a087cd03843
Clint Jones <clinton.alan.jones@gmail.com>
2025-06-02 14:35:10 -0700

    Planning for postgres replacement using blueprints

---
+++ iac/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Source the variables: . run.sh
+export RENDER_API_TOKEN="rnd_uRvI4rAIb0nkz2zr5PAB83HgJBJ9"
+export OWNER_ID="tea-ct93ssl6l47c73amic00"
+export REGION="oregon"
+
+echo $RENDER_API_TOKEN
+echo $OWNER_ID
+echo $REGION
